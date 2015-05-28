df443b6f7eb96276d7b897378b841677b2a41ae3
MajicPris <ksharutin@gmail.com>
2015-05-28 23:55:42 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include"string.h"
+using namespace std;
+
+int main()
+{
+	/*char stroka1[1000], stroka2[1000];
+	cin.get(stroka1, 1000);
+	cin.get();
+	cin.get(stroka2, 1000);
+
+	String title(stroka1);
+	title + " ";
+	title + stroka2;
+
+
+
+	title.showstring();
+	//if (title < title2)
+	{
+		cout << "bolshe";
+	}*/
+	String string1("asdddddd");
+	String string2("vvvvvv");
+	String string3("s");
+	string1.showstring();
+	string1 + string2;
+	string1.showstring();
+	system("PAUSE");
+	return 0;
+}
